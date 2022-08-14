09df9ca1c3fdd474af651c7fd1e486ed6ea0765d
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-08-14 11:55:14 +0430

    feat(): add dark and light button

--- src/features/Settings/components/Preferences/Preferences.tsx
+++ src/features/Settings/components/Preferences/Preferences.tsx
@@ -1,4 +1,5 @@
 import { Divider, Radio } from "antd";
+import classnames from "classnames";
 import classNames from "classnames";
 import styles from "./preferences.module.css";
 
@@ -24,6 +25,22 @@ export function Preferences() {
 
       <div className={styles.item}>
         <h2> Themes</h2>
+
+        <Radio.Group
+          className={styles.colorGroup}
+          name="radiogroup"
+          defaultValue={1}
+        >
+          <Radio.Button
+            className={classnames({
+              [styles.themeBtn]: true,
+              [styles.themeBtnLight]: true,
+            })}
+          >
+            Light
+          </Radio.Button>
+          <Radio.Button className={styles.themeBtn}>Dark</Radio.Button>
+        </Radio.Group>
       </div>
     </div>
   );
