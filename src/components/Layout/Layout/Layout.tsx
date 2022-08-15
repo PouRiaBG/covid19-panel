fb518ce6a633ad419d3f3702ec6770623938a549
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-08-15 11:02:55 +0430

    feat(): implement sidebar for mobile

--- src/components/Layout/Layout/Layout.tsx
+++ src/components/Layout/Layout/Layout.tsx
@@ -2,12 +2,15 @@ import { Outlet } from "react-router";
 import { SideBar } from "../SideBar/SideBar";
 import styles from "./layout.module.css";
 import { Header } from "../Header/Header";
+import { useScreen } from "../../../hooks/useScreen";
+import { SideBarMobile } from "../SideBarMobile/SideBarMobile";
 function Layout() {
+  const { isMobile } = useScreen();
   return (
     <div className={styles.container}>
-      <SideBar />
+      {isMobile ? <SideBarMobile /> : <SideBar />}
       <div className={styles.main}>
-        <Header />
+        {isMobile ? null : <Header />}
         <Outlet />
       </div>
     </div>
---
+++ src/components/Layout/SideBarMobile/SideBarMobile.tsx
@@ -0,0 +1,83 @@
+import {
+  MenuOutlined,
+  SettingOutlined,
+  UserSwitchOutlined,
+} from "@ant-design/icons";
+import { Button, Drawer } from "antd";
+import { useState } from "react";
+import { useSideRoutes } from "../../../hooks/useSideRoutes";
+import styles from "./sidebarmobile.module.css";
+import { NavLink } from "react-router-dom";
+
+export function SideBarMobile() {
+  const [visible, setVisible] = useState(false);
+  const ROUTES = useSideRoutes();
+  const onClose = () => {
+    setVisible(false);
+  };
+  const onClickHandler = () => {
+    setVisible(true);
+  };
+  return (
+    <div className={styles.container}>
+      <Button
+        onClick={onClickHandler}
+        className={styles.icon}
+        size="large"
+        icon={<MenuOutlined />}
+      />
+      <Drawer
+        title="Sibar Menu"
+        placement="left"
+        onClose={onClose}
+        visible={visible}
+        drawerStyle={{
+          width: "20rem",
+        }}
+      >
+        <div className={styles.itemsContainer}>
+          <div className={styles.items}>
+            {ROUTES.map((item) => {
+              return (
+                <NavLink
+                  key={item.path}
+                  to={item.path}
+                  className={({ isActive }) =>
+                    isActive ? styles.activeItem : undefined
+                  }
+                >
+                  <Button icon={item.icon} type="text" size="large">
+                    {item.name}
+                  </Button>
+                </NavLink>
+              );
+            })}
+          </div>
+
+          <div className={styles.items}>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.activeItem : undefined
+              }
+              to="F&Q"
+            >
+              <Button icon={<UserSwitchOutlined />} type="text" size="large">
+                F&Q
+              </Button>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.activeItem : styles.test
+              }
+              to="settings"
+            >
+              <Button icon={<SettingOutlined />} type="text" size="large">
+                Settings
+              </Button>
+            </NavLink>
+          </div>
+        </div>
+      </Drawer>
+    </div>
+  );
+}
