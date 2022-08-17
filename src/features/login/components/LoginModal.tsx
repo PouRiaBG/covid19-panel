030ad4d4ce966ef9e4b872cd6755051c9a5c2c5f
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-08-17 11:29:42 +0430

    feat: implement Login modal WIP

--- src/features/login/components/LoginModal.tsx
+++ src/features/login/components/LoginModal.tsx
@@ -1,9 +1,19 @@
-import { Button, Input, Modal } from "antd";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Button, Form, Input, Modal } from "antd";
 import { useNavigate } from "react-router-dom";
 import styles from "./loginmodal.module.css";
 
 export function LoginModal() {
   const navigate = useNavigate();
+  const { mutate } = useMutation(async () => {
+    return fetch("/login", { method: "POST" }).then((response) =>
+      console.log({ response })
+    );
+  });
+
+  const onFinish = (values: any) => {
+    mutate(values);
+  };
   return (
     <Modal
       title="Modal"
@@ -14,10 +24,15 @@ export function LoginModal() {
       className={styles.testClass}
       wrapClassName={styles.testWrap}
     >
-      <Input type="text" placeholder="username " />
+      <Form onFinish={onFinish}>
+        <Form.Item name="username">
+          <Input type="text" placeholder="username " />
+        </Form.Item>
 
-      <Input.Password placeholder="password" />
-      <Button>Login</Button>
+        <Form.Item>
+          <Button htmlType="submit">Login</Button>
+        </Form.Item>
+      </Form>
     </Modal>
   );
 }
