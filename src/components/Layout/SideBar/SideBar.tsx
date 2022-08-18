f536bfb82d3a8bbe37cf441697a6ba712bf56f0b
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-08-18 17:06:38 +0430

    refactor: use media query for responsive sidabar tablet and desktop

--- src/components/Layout/SideBar/SideBar.tsx
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -1,26 +1,22 @@
 import styles from "./sideBar.module.css";
 import { Button } from "antd";
 import { SettingOutlined, UserSwitchOutlined } from "@ant-design/icons";
-import { useScreen } from "@hooks/useScreen";
 import classnames from "classnames";
 import { useSideRoutes } from "@hooks/useSideRoutes";
-import Link from "next/link";
 import { NavLink } from "@components/NavLink/NavLink";
+
 export function SideBar() {
-  const { isDesktop, isTablet } = useScreen();
   const ROUTES = useSideRoutes();
   return (
     <aside
       className={classnames({
         [styles.container]: true,
-        [styles.containerTablet]: isTablet,
       })}
     >
       <div className={styles.list}>
         <div
           className={classnames({
             [styles.header]: true,
-            [styles.headerTablet]: isTablet,
           })}
         >
           <h1>.Stats</h1>
@@ -28,7 +24,6 @@ export function SideBar() {
         <div
           className={classnames({
             [styles.items]: true,
-            [styles.itemsTablet]: isTablet,
           })}
         >
           {ROUTES.map((item) => {
@@ -39,53 +34,51 @@ export function SideBar() {
                 key={item.path}
                 pathname={item.path}
               >
-                {isDesktop && (
+                <div className={styles.desktopButtons}>
                   <Button icon={item.icon} type="text" size="large">
                     {item.name}
                   </Button>
-                )}
+                </div>
 
-                {isTablet && (
+                <div className={styles.tabletButtons}>
                   <Button icon={item.icon} type="text" size="large" />
-                )}
+                </div>
               </NavLink>
             );
           })}
         </div>
       </div>
       <div
-        suppressHydrationWarning={true}
         className={classnames({
           [styles.items]: true,
-          [styles.itemsTablet]: isTablet,
+          [styles.desktopButtons]: true,
           [styles.footerItems]: true,
         })}
       >
-        {isDesktop && (
-          <>
-            <NavLink activeClass={styles.activeItem} pathname="/F&Q">
-              <Button icon={<UserSwitchOutlined />} type="text" size="large">
-                F&Q
-              </Button>
-            </NavLink>
-            <NavLink activeClass={styles.activeItem} pathname="/settings">
-              <Button icon={<SettingOutlined />} type="text" size="large">
-                Settings
-              </Button>
-            </NavLink>
-          </>
-        )}
-
-        {isTablet && (
-          <>
-            <NavLink activeClass={styles.activeItem} pathname="/F&Q">
-              <Button icon={<UserSwitchOutlined />} type="text" size="large" />
-            </NavLink>
-            <NavLink activeClass={styles.activeItem} pathname="/settings">
-              <Button icon={<SettingOutlined />} type="text" size="large" />
-            </NavLink>
-          </>
-        )}
+        <NavLink activeClass={styles.activeItem} pathname="/F&Q">
+          <Button icon={<UserSwitchOutlined />} type="text" size="large">
+            F&Q
+          </Button>
+        </NavLink>
+        <NavLink activeClass={styles.activeItem} pathname="/settings">
+          <Button icon={<SettingOutlined />} type="text" size="large">
+            Settings
+          </Button>
+        </NavLink>
+      </div>
+      <div
+        className={classnames({
+          [styles.items]: true,
+          [styles.tabletButtons]: true,
+          [styles.footerItems]: true,
+        })}
+      >
+        <NavLink activeClass={styles.activeItem} pathname="/F&Q">
+          <Button icon={<UserSwitchOutlined />} type="text" size="large" />
+        </NavLink>
+        <NavLink activeClass={styles.activeItem} pathname="/settings">
+          <Button icon={<SettingOutlined />} type="text" size="large" />
+        </NavLink>
       </div>
     </aside>
   );
