25f5231d17db17c34267c9c10a5ea75127354a59
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-08-18 12:48:07 +0430

    feat: implement react query for  hydration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import type { AppProps } from "next/app";
 import React from "react";
 import "antd/dist/antd.min.css";
 import "../styles/globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { Layout } from "../src/components";
 require("../src/mocks");
 
@@ -10,9 +14,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Hydrate>
     </QueryClientProvider>
   );
 }
