ad545c210fa5bc9ccf8289ec73d64db60320f594
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-09-01 19:54:55 +0430

    fix: fix key problem of react query

--- src/features/Statistics/api/useGetStats.ts
+++ src/features/Statistics/api/useGetStats.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { AllStats } from "../types";
 
 export const useGetStats = () => {
-  return useQuery<AllStats>(["countries"], async () => {
+  return useQuery<AllStats>(["stats"], async () => {
     return fetch("https://disease.sh/v3/covid-19/all")
       .then((res) => res.json())
       .then((finalData) => finalData);
