9716e1a06d98977bdff06946b3a683c6513d3d4c
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-09-01 19:59:13 +0430

    feat: add enable and disable option

--- src/hooks/useSideRoutes.tsx
+++ src/hooks/useSideRoutes.tsx
@@ -3,6 +3,7 @@ import {
   AppstoreOutlined,
   UserSwitchOutlined,
   ExclamationCircleOutlined,
+  EditOutlined,
 } from "@ant-design/icons";
 
 export const useSideRoutes = () => {
@@ -11,24 +12,32 @@ export const useSideRoutes = () => {
       name: "Dashboard",
       path: "/",
       icon: <BarChartOutlined />,
+      isDisabled: false,
     },
 
     {
       name: "Countries",
       path: "countries",
       icon: <AppstoreOutlined />,
+      isDisabled: false,
     },
-
-    {
-      name: "Vaccine",
-      path: "vaccine",
-      icon: <UserSwitchOutlined />,
-    },
-
     {
       name: "News",
       path: "news",
       icon: <ExclamationCircleOutlined />,
+      isDisabled: false,
+    },
+    {
+      name: "Vaccine",
+      path: "vaccine",
+      icon: <UserSwitchOutlined />,
+      isDisabled: false,
+    },
+    {
+      name: "Test Yourself",
+      path: "testcovid",
+      icon: <EditOutlined />,
+      isDisabled: true,
     },
   ];
   return ROUTES;
