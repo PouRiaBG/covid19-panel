4852ce9d89f0179bc41eb22b67edc045bd3e6e0f
PouRiaBG <pooria.bonyadgozar@gmail.com>
2022-09-03 00:30:41 +0430

    feat: add authentication condition

--- src/components/Layout/SideBar/SideBar.tsx
+++ src/components/Layout/SideBar/SideBar.tsx
@@ -5,8 +5,10 @@ import { NavLink } from "react-router-dom";
 import { useScreen } from "../../../hooks/useScreen";
 import classnames from "classnames";
 import { useSideRoutes } from "../../../hooks/useSideRoutes";
+import { useAuthStore } from "../../../store/authStore";
 export function SideBar() {
   const { isDesktop, isTablet } = useScreen();
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const ROUTES = useSideRoutes();
   return (
     <aside
@@ -31,6 +33,7 @@ export function SideBar() {
           })}
         >
           {ROUTES.map((item) => {
+            if (item.isAdmin) return;
             return (
               <NavLink
                 data-testid={item.name}
@@ -71,55 +74,25 @@ export function SideBar() {
           [styles.footerItems]: true,
         })}
       >
-        {/* <NavLink
-          className={({ isActive }) =>
-            isActive ? styles.activeItem : undefined
-          }
-          to="F&Q"
-        >
-          <Button icon={<UserSwitchOutlined />} type="text" size="large">
-            F&Q
-          </Button>
-        </NavLink> */}
-        <NavLink
-          data-testid="Settings"
-          className={({ isActive }) =>
-            isActive ? styles.activeItem : styles.test
-          }
-          to="settings"
-        >
-          {isDesktop && (
-            <Button icon={<SettingOutlined />} type="text" size="large">
-              Settings
-            </Button>
-          )}
+        {isAuthenticated ? (
+          <NavLink
+            data-testid="Settings"
+            className={({ isActive }) =>
+              isActive ? styles.activeItem : styles.test
+            }
+            to="settings"
+          >
+            {isDesktop && (
+              <Button icon={<SettingOutlined />} type="text" size="large">
+                Settings
+              </Button>
+            )}
 
-          {isTablet && (
-            <Button icon={<SettingOutlined />} type="text" size="large" />
-          )}
-        </NavLink>
-
-        {/* {isTablet && (
-          <>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? styles.activeItem : undefined
-              }
-              to="F&Q"
-            >
-              <Button icon={<UserSwitchOutlined />} type="text" size="large" />
-            </NavLink>
-            <NavLink
-              data-testid="Settings"
-              className={({ isActive }) =>
-                isActive ? styles.activeItem : styles.test
-              }
-              to="settings"
-            >
+            {isTablet && (
               <Button icon={<SettingOutlined />} type="text" size="large" />
-            </NavLink>
-          </>
-        )} */}
+            )}
+          </NavLink>
+        ) : null}
       </div>
     </aside>
   );
--- src/hooks/useSideRoutes.tsx
+++ src/hooks/useSideRoutes.tsx
@@ -5,14 +5,17 @@ import {
   ExclamationCircleOutlined,
   EditOutlined,
 } from "@ant-design/icons";
+import { useAuthStore } from "../store/authStore";
 
 export const useSideRoutes = () => {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
   const ROUTES = [
     {
       name: "Dashboard",
       path: "/",
       icon: <BarChartOutlined />,
       isDisabled: false,
+      isAdmin: false,
     },
 
     {
@@ -20,24 +23,29 @@ export const useSideRoutes = () => {
       path: "countries",
       icon: <AppstoreOutlined />,
       isDisabled: false,
+      isAdmin: false,
     },
     {
       name: "News",
       path: "news",
       icon: <ExclamationCircleOutlined />,
       isDisabled: false,
+      isAdmin: false,
     },
     {
       name: "Vaccine",
       path: "vaccine",
       icon: <UserSwitchOutlined />,
       isDisabled: false,
+
+      isAdmin: !isAuthenticated,
     },
     {
       name: "Test Yourself",
       path: "testcovid",
       icon: <EditOutlined />,
       isDisabled: true,
+      isAdmin: !isAuthenticated,
     },
   ];
   return ROUTES;
